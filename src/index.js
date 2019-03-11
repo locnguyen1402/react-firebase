9c0e9191d127017e495179f23bc563def3a45768
Khanh <kknvnn29@gmail.com>
2019-03-11 14:01:37 +0700

    add react bootstrap

--- src/index.js
+++ src/index.js
@@ -8,23 +8,38 @@ import Home from './home/index'
 import Detail from './detail/index'
 import Products from './products/index'
 
+import Nav from 'react-bootstrap/Nav';
+
 const routing = (
     <Router>
         <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/detail">Detail</Link>
-                </li>
-            </ul>
-            <Route exact path={"/"} component={Home}/>
-            <Route path={"/products"} component={Products}/>
-            <Route path={"/detail"} component={Detail}/>
+          {/*<ul>*/}
+            {/*<li>*/}
+              {/*<Link to="/">Home</Link>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+              {/*<Link to="/products">Products</Link>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+              {/*<Link to="/detail">Detail</Link>*/}
+            {/*</li>*/}
+          {/*</ul>*/}
+          <Nav variant="tabs" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/products" href="/products">Products</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/detail" href="/detail" disabled>
+                Detail
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Route exact path={"/"} component={Home}/>
+          <Route path={"/products"} component={Products}/>
+          <Route path={"/detail"} component={Detail}/>
         </div>
     </Router>
 )
